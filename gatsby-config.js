a108f60fcd425c40f212568c9cb90344a055adb2
Abbi Kissee <abbikissee@Abbis-MBP.attlocal.net>
2022-12-03 09:32:39 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `ResourceSite`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
